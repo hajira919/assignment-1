8384482440a595ddb96d7c3b9b28c44d346ac532
hajra <hajirashahzad09@gmail.com>
2025-10-03 21:43:15 +0500

    calculator

---
+++ assignment 1/Class2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    internal class Class2
+    {
+        static void Main(string[]args)
+        {
+            double num1, num2, result = 0;
+            char operation;
+
+            Console.WriteLine("=== Simple Calculator ===");
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nChoose an operation:");
+            Console.WriteLine("+ for Addition");
+            Console.WriteLine("- for Subtraction");
+            Console.WriteLine("* for Multiplication");
+            Console.WriteLine("/ for Division");
+            Console.Write("Enter your choice: ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine($"Result = {result}");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine($"Result = {result}");
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine($"Result = {result}");
+                    break;
+
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"Result = {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed!");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operation! Please choose +, -, * or /.");
+                    break;
+            }
+          
+        }
+    }
+}
+
+
+           
+        
