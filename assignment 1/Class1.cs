0cb99fc9995d1998f5f7b6e379ae68f39a1bc606
hajra <hajirashahzad09@gmail.com>
2025-10-03 21:04:03 +0500

    like Added even/odd check

---
+++ assignment 1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is Even.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is Odd.");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
--- assignment 1/Program.cs
+++ assignment 1/Program.cs
@@ -8,8 +8,8 @@ namespace assignment_1
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+       
+        
+        
+     }
 }
